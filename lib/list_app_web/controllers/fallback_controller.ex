14a71c708539f20537a7dc014be0b90450791a87
Wil Stephens <wilr.stephens@gmail.com>
2019-05-25 22:51:47 -0500

    add user context (#3)

---
+++ lib/list_app_web/controllers/fallback_controller.ex
@@ -0,0 +1,22 @@
+defmodule ListAppWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use ListAppWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(ListAppWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(ListAppWeb.ErrorView)
+    |> render(:"404")
+  end
+end
